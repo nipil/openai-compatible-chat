9af354997c634a29aea137b5d6eff693e01e6cd1
nipil <nipil@users.noreply.github.com>
2026-04-26 15:20:59 +0200

    Regroup web fn to handle chat and produce SSE chat events

--- native/src/web.rs
+++ native/src/web.rs
@@ -1,7 +1,5 @@
 use std::convert::Infallible;
 
-use async_openai::Client;
-use async_openai::config::OpenAIConfig;
 use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response, sse};
@@ -15,8 +13,6 @@ use tracing::{error, instrument};
 
 use crate::AppState;
 use crate::openai::{ProviderError, get_chat_event, send_chat_request};
-
-const SSE_EVENT_ERROR: &str = "error";
 
 #[derive(Error, Debug)]
 enum WebError {
@@ -32,7 +28,6 @@ impl IntoResponse for WebError {
         let status = match self {
             Self::Api(_) => StatusCode::INTERNAL_SERVER_ERROR,
             Self::Forbidden(_) => StatusCode::FORBIDDEN,
-            // Self::Io(_) =>
         };
         (status, self.to_string()).into_response()
     }
@@ -77,6 +72,7 @@ fn router(state: AppState, dist_wasm: &str) -> Router {
 
 // ── GET /api/config ───────────────────────────────────────────────────────────
 
+#[instrument(skip_all)]
 async fn handle_config(State(s): State<AppState>) -> Json<ConfigDto> {
     Json(ConfigDto {
         prepend_system_prompt: s.prepend_system_prompt.as_ref().clone(),
@@ -85,6 +81,7 @@ async fn handle_config(State(s): State<AppState>) -> Json<ConfigDto> {
 
 // ── GET /api/models ───────────────────────────────────────────────────────────
 
+#[instrument(skip_all)]
 async fn handle_models(State(s): State<AppState>) -> Json<Vec<ModelDto>> {
     Json(
         s.candidate_models
@@ -99,12 +96,13 @@ async fn handle_models(State(s): State<AppState>) -> Json<Vec<ModelDto>> {
 
 // ── POST /api/chat ────────────────────────────────────────────────────────────
 
-// TODO: clarify the base-errors that are used
+#[instrument(skip_all)]
 async fn handle_chat(
     State(s): State<AppState>,
     Json(mut chat): Json<ChatRequest>,
 ) -> Result<sse::Sse<stream::BoxStream<'static, Result<sse::Event, Infallible>>>, WebError> {
-    // CRITICAL: server-side check that the client is not trying to jail out
+    // CRITICAL/SECURITY
+    // server-side check that the client is not trying to jail out
     if s.candidate_models.get(&chat.model).is_none() {
         Err(WebError::Forbidden(format!(
             "Configuration does not allow model '{}'",
@@ -139,49 +137,37 @@ async fn handle_chat(
         }
     }
 
-    let stream: stream::BoxStream<'static, Result<sse::Event, Infallible>> =
-        // only awaits the setup (the initial API call to get the stream handle)
-        // no chunk are processed yet and wil be done by the caller
-        // which, as the stream is Send + 'static, can be an async fn (axum)
+    // Can only fail here during initial request (setup)
+    let openai_stream = send_chat_request(s.openai_client.as_ref(), &chat).await?;
 
-        match process_chat_stream(s.openai_client.as_ref(), &chat).await {
-            Ok(s) => s,
-            Err(e) => {
-                // INFORMATION
-                // Here we can not get any Error from the chunk
-                // processing future, as it is managed from its
-                // SSE stream then() closure. And the stream
-                // is not even streamed here, as we return it back
-                // to our caller, which is an Axum route handler.
-                // And anyway, that closure is marked as Infallible
-                // anyway, as it only returns Ok and rust knows it !
-
-                // IMPORTANT
-                // During the creation of the SSE stream itself,
-                // we could get errors which we can handle:
-                // - msg_to_api() could fail as these could :
-                //   - ChatCompletionRequestSystemMessageArgs.build()
-                //   - ChatCompletionRequestAssistantMessageArgs.build()
-                //   - ChatCompletionRequestUserMessageArgs.build()
-                // - CreateChatCompletionRequestArgs.build()
-                // But as we build them from default() and only add
-                // valid stuff, it most likely could not anyway. But
-                // there is no reason the async_openai crate could not
-                // fail on its own, so we have to handle this anyway.
-                Box::pin(futures::stream::once(async move {
-                    // TODO: logging ?
-                    // On server-side error **DURING SSE CREATION**,
-                    // send an SSE "error event" to notify the client
-                    Ok(sse::Event::default()
-                        .event(SSE_EVENT_ERROR)
-                        .data(e.to_string()))
-                }))
+    // Every time a chunk is ready, the provided FnMut will be called
+    // on it and and return a *Future* (axum needs a future) to poll,
+    // which will then be run to completion to produce sse::Event
+    let sse_stream = openai_stream.then(
+        // the returned stream must be 'static because we Box::pin it,
+        // so the closure cannot borrow from the enclosing scope, and
+        // should own everything it references (hence this 'move')
+        move |chunk| {
+            // returns an async block the caller will need to await, and
+            // async must be 'static to be polled, so 'move' to own
+            async move {
+                // as seen in function signature, this returns a Result
+                // of sse::Event and Infallible, so no Err? possible
+                // To report error during chat processing :
+                // - server-side : use log with higher severity
+                // - client-side : send ChatEvent::Error via SSE
+                let event = get_chat_event(chunk);
+                Ok(SseEventOut::from(event).into())
             }
-        };
+        },
+    );
 
-    // we return an actual stream, which axum will iterate over, and
-    // each time, will pull one chunk, run the closure asynchronously
-    Ok(sse::Sse::new(stream))
+    // Fixed memory is needed so that it can safely be referenced and
+    // polled asynchronously. So move the stream onto the heap (Box)
+    // and mark it as fixed in memory space
+    // And we return it as a box trait object "without a lifetime" ('static)
+    // so that it can live for as long as necessary in the async event loop
+    Ok(sse::Sse::new(Box::pin(sse_stream)))
 }
 
 // ── SSE Event helper ──────────────────────────────────────────────────────────
@@ -238,43 +224,4 @@ impl SseEventOut {
     fn into_inner(self) -> ChatEvent {
         self.0
     }
-}
-
-/// One request to the provider (only initial request, not streaming response)
-#[instrument(level = "trace", skip_all)]
-async fn process_chat_stream(
-    client: &Client<OpenAIConfig>,
-    chat: &ChatRequest,
-) -> Result<stream::BoxStream<'static, Result<sse::Event, Infallible>>, WebError> {
-    // TODO: refactor same as cli ? into openai
-
-    // This can fail during the setup time (ProviderError)
-    let openai_stream = send_chat_request(client, chat).await?;
-
-    // Every time a chunk is ready, the provided FnMut will be called
-    // on it and and return a *Future* (axum needs a future) to poll,
-    // which will then be run to completion to produce sse::Event
-    let sse_event_stream = openai_stream.then(
-        // the returned stream must be 'static because we Box::pin it,
-        // so the closure cannot borrow from the enclosing scope, and
-        // should own everything it references.
-        move |chunk| {
-            // returns an async block the caller will need to await, and
-            // async must be 'static to be polled, so own its references
-            async move {
-                // as seen in function signature, this returns a
-                // Result<sse::Event, Infallible> so no Err? bubbling.
-                // To report error during chat processing
-                // - server-side : use logging
-                // - client-side : use ChatEvent::Error
-                let event = get_chat_event(chunk);
-                Ok(SseEventOut::from(event).into())
-            }
-        },
-    );
-
-    // Fixed memory is needed so that it can safely be referenced and polled.
-    // So move the stream onto the heap (Box) and mark it as fixed in memory
-    // space, which requires it to be 'static too (afaik, see above ?)
-    Ok(Box::pin(sse_event_stream))
 }
